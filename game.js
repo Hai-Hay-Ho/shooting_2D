5bbafe32b8173282865ade231deb2255b4538f0c
Hai-Hay-Ho <hominhhai2k4@gmail.com>
2026-02-11 23:27:01 +0700

    reponsive mobile

--- game.js
+++ game.js
@@ -14,9 +14,13 @@ let currentUser = null;
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
     parent: 'game-container',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade: {
